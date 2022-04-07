79b4154a81f23307b735fa34abb1812a16ff42af
Rafael Soncine <rafinhaa@me.com>
2022-04-07 11:31:48 -0300

    App de pedido de pizzas - Estrutura do projeto - Tema da aplicação

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { View } from "react-native";
 import { useFonts, DMSans_400Regular } from "@expo-google-fonts/dm-sans";
 import { DMSerifDisplay_400Regular } from "@expo-google-fonts/dm-serif-display";
 import AppLoading from "expo-app-loading";
+import { ThemeProvider } from "styled-components/native";
+import theme from "./src/theme";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -14,5 +15,5 @@ export default function App() {
     return <AppLoading />;
   }
 
-  return <View></View>;
+  return <ThemeProvider theme={theme}></ThemeProvider>;
 }
