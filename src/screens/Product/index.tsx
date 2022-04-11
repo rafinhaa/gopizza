e6df0ab2685a232be25848a4acb3730773ee4dec
Rafael Soncine <rafinhaa@me.com>
2022-04-11 11:21:52 -0300

    App de pedido de pizzas - Produto - Header de produto

--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -1,12 +1,21 @@
 import React from "react";
-import { Platform } from "react-native";
+import { Platform, TouchableOpacity } from "react-native";
 
-import { Container } from "./styles";
+import { Container, Header, Title, DeleteLabel } from "./styles";
 
 const Product: React.FC = () => {
   const behavior = Platform.OS === "ios" ? "padding" : undefined;
 
-  return <Container behavior={behavior}></Container>;
+  return (
+    <Container behavior={behavior}>
+      <Header>
+        <Title>Cadastrar</Title>
+        <TouchableOpacity>
+          <DeleteLabel>Deletar</DeleteLabel>
+        </TouchableOpacity>
+      </Header>
+    </Container>
+  );
 };
 
 export default Product;
--- src/screens/Product/styles.ts
+++ src/screens/Product/styles.ts
@@ -1,6 +1,34 @@
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
+import { LinearGradient } from "expo-linear-gradient";
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
 
 export const Container = styled.KeyboardAvoidingView`
   flex: 1;
   background-color: ${({ theme: { COLORS } }) => COLORS.BACKGROUND};
+`;
+
+export const Header = styled(LinearGradient).attrs(({ theme: { COLORS } }) => ({
+  colors: COLORS.GRADIENT,
+}))`
+  width: 100%;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: ${getStatusBarHeight() + 33}px 20px 24px;
+`;
+
+export const Title = styled.Text`
+  font-size: 24px;
+  ${({ theme: { COLORS, FONTS } }) => css`
+    font-family: ${FONTS.TITLE};
+    color: ${COLORS.TITLE};
+  `}
+`;
+
+export const DeleteLabel = styled.Text`
+  font-size: 14px;
+  ${({ theme: { COLORS, FONTS } }) => css`
+    font-family: ${FONTS.TEXT};
+    color: ${COLORS.TITLE};
+  `}
 `;
