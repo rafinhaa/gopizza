c8bc044d0e847a2054a1b592247e6f50b9e21f8a
Rafael Soncine <rafinhaa@me.com>
2022-04-12 10:15:01 -0300

    App de pedido de pizzas - Produto - Utilizando o ImagePicker

--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { Platform, TouchableOpacity } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import { ImageInfo } from "expo-image-picker/build/ImagePicker.types";
+
 import ButtonBack from "../../components/ButtonBack";
 import Photo from "../../components/Photo";
 
@@ -14,6 +17,22 @@ import {
 
 const Product: React.FC = () => {
   const behavior = Platform.OS === "ios" ? "padding" : undefined;
+  const [image, setImage] = useState("");
+
+  async function handlePickerImage() {
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (status === "granted") {
+      const pickerResult = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        aspect: [4, 3],
+      });
+      if (!pickerResult.cancelled) {
+        const { uri } = pickerResult as ImageInfo;
+        setImage(uri);
+      }
+    }
+  }
 
   return (
     <Container behavior={behavior}>
@@ -25,8 +44,12 @@ const Product: React.FC = () => {
         </TouchableOpacity>
       </Header>
       <Upload>
-        <Photo uri="" />
-        <PickImageButton title="Carregar" type="secondary" />
+        <Photo uri={image} />
+        <PickImageButton
+          title="Carregar"
+          type="secondary"
+          onPress={handlePickerImage}
+        />
       </Upload>
     </Container>
   );
