aeecfad940775a143e0b95854b781d26f6878802
Rafael Soncine <rafinhaa@me.com>
2022-04-15 12:01:35 -0300

    App de pedido de pizzas - Produto - Tipagem das Rotas

---
+++ src/@types/navigation.d.ts
@@ -0,0 +1,7 @@
+import { RootStackParamList } from "../routes/user.stack.routes";
+
+export declare global {
+  namespace ReactNavigation {
+    interface RootParamList extends RootStackParamList {}
+  }
+}
