af55f63756218d6ba1731be1f413677b6a6b9db9
Rafinhaa <rafinhaa@me.com>
2022-04-20 10:01:06 -0300

    App de pedido de pizzas - Pedido - Header do pedido

--- src/screens/Order/index.tsx
+++ src/screens/Order/index.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import { Platform } from "react-native";
 
-import { Container } from "./styles";
+import { Container, Header } from "./styles";
+
+import ButtonBack from "../../components/ButtonBack";
 
 const Order: React.FC = () => {
   const behavior = Platform.OS === "ios" ? "padding" : undefined;
 
-  return <Container behavior={behavior}></Container>;
+  return (
+    <Container behavior={behavior}>
+      <Header>
+        <ButtonBack style={{ marginBottom: 108 }} />
+      </Header>
+    </Container>
+  );
 };
 
 export default Order;
